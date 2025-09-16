66274bdcd719fc88bf4ac9053b78212ef91cd480
Istomin Alexey <tmozzze@gmail.com>
2025-09-16 21:13:34 +0300

    ADDED: Euclidean Algorithm

--- LeetCode/2197/main.go
+++ LeetCode/2197/main.go
@@ -5,11 +5,11 @@ import "fmt"
 func main() {
 	n1 := 90
 	n2 := 84
-	fmt.Println(EuclideanAlg(n1, n2))
+	fmt.Println(EuclideanAlg(n2, n1))
 }
 
 func EuclideanAlg(n1, n2 int) int {
-	rem := 1
+	var rem int = 1
 
 	if n1 > n2 {
 		for rem != 0 {
@@ -17,11 +17,13 @@ func EuclideanAlg(n1, n2 int) int {
 			n1 = n2
 			n2 = rem
 		}
-		return n2
+		return n1
 	}
-	rem = n2 % n1
-	n2 = n1
-	n1 = rem
+	for rem != 0 {
+		rem = n2 % n1
+		n2 = n1
+		n1 = rem
+	}
 
-	return n1
+	return n2
 }
