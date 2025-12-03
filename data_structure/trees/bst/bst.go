1288181978141743807f4089b13acdb6d490b6d4
Istomin Alexey <tmozzze@gmail.com>
2025-12-03 03:00:32 +0300

    ADDED: trying to create bst

---
+++ data_structure/trees/bst/bst.go
@@ -0,0 +1,37 @@
+package bst
+
+type Node struct {
+	Key   int
+	Left  *Node
+	Right *Node
+}
+
+type BST struct {
+	Root *Node
+}
+
+func NewBST() *BST {
+	return &BST{Root: nil}
+}
+
+func (t *BST) Insert(key int) {
+	t.Root = insert(t.Root, key)
+}
+
+func insert(node *Node, key int) *Node {
+	if node == nil {
+		return &Node{Key: key}
+	}
+
+	if key < node.Key {
+		node.Left = insert(node.Left, key)
+	} else if key > node.Key {
+		node.Right = insert(node.Right, key)
+	}
+
+	return node
+}
+
+// func (t *BST) Delete(key int) bool {
+// 	t.Root = delete(t.Root, key)
+// }
